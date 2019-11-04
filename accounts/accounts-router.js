0b61e85985afe459a3fd66d55a7fcdbc38164f3f
Chazaray Robinson <chaarayrobinson@Chazarays-MacBook-Pro.local>
2019-11-04 16:03:55 -0500

    initial commit

---
+++ accounts/accounts-router.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const knex = require('../data/dbConfig.js');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+
+})
+
+module.exports = router;
